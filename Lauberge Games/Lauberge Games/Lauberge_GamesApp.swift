462b2dbb84168a4bf00ed2e0d3100c1d71628d28
Dias <dias.atudinov@gmail.com>
2025-05-13 16:39:32 +0500

    app setup

---
+++ Lauberge Games/Lauberge Games/Lauberge_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lauberge_GamesApp.swift
+//  Lauberge Games
+//
+//  Created by Dias Atudinov on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lauberge_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
